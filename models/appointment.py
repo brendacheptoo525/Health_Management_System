95d469638fe4d4385db98f5c230cccbcb4b98a9f
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-06-12 17:09:09 +0300

    Create appointment function

--- models/appointment.py
+++ models/appointment.py
@@ -11,3 +11,11 @@ class Appointment(Base):
     patient_id = Column(Integer, ForeignKey('patient.id'))
     doctor_id = Column(Integer, ForeignKey('doctor.id'))
 
+def create_appointment(session, appointment_date, reason, status, patient_id, doctor_id):
+    try:
+        appointment = Appointment(appointment_date=appointment_date, reason=reason, status=status, patient_id=patient_id, doctor_id=doctor_id)
+        session.add(appointment)
+        session.commit()
+        print("Appointment created successfully.")
+    except Exception as e:
+        print("Error occurred while creating appointment:", e)
