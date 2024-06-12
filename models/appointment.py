ce060504128c32085a855951174409919b09ede5
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-06-12 17:07:52 +0300

    Appointment class definition

--- models/appointment.py
+++ models/appointment.py
@@ -1,2 +1,13 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from database.connection import Base
+
+class Appointment(Base):
+    _tablename_ = 'appointment'
+
+    id = Column(Integer, primary_key=True, index=True)
+    appointment_date = Column(String, nullable=False)
+    reason = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    patient_id = Column(Integer, ForeignKey('patient.id'))
+    doctor_id = Column(Integer, ForeignKey('doctor.id'))
+
