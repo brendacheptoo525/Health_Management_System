0a8bd2eb12ab0588e367a3751e9dac581aa765ff
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-06-12 17:16:25 +0300

    Class doctor definition

--- models/doctor.py
+++ models/doctor.py
@@ -1,3 +1,13 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from database.connection import Base
+
+class Doctor(Base):
+    _tablename_ = 'doctor'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    speciality = Column(String, nullable=False)
+    contact = Column(String, nullable=False)
+
+    patients = relationship("Patient", back_populates="doctor")
